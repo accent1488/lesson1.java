c66641d80957f6b8e678372d3c3dcfad092f6af0
accent1488 <rkfldkd@gmail.com>
2021-07-15 02:56:54 +0400

    lesson14+15+16

--- src/ClassesAndObjects14.java
+++ src/ClassesAndObjects14.java
@@ -3,17 +3,24 @@ public class ClassesAndObjects14 {
         Person person1 = new Person();
         person1.name = "Romeo";
         person1.age = 33;
-        person1.sayHello();
         Person person2 = new Person();
         person2.name = "Vlad";
         person2.age = 20;
-        person2.speak();
+        int year1 = person1.calculateYearsToRetirement();
+        int year2 = person2.calculateYearsToRetirement();
+        System.out.println("первому человеку до пенсии " +year1+" лет");
+        System.out.println("а второму " +year2+" лет");
     }
 }
 
 class Person{
     String name;
     int age;
+
+    int calculateYearsToRetirement(){
+        int years = 65-age;
+        return years;
+    }
 
     void speak(){
         for (int i = 2; i<3; i++)
