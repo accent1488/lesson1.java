eb936e3075271db85d182f7ada1807d3998275a5
accent1488 <rkfldkd@gmail.com>
2021-07-14 19:30:11 +0400

    lesson 5,6,7,8,9,10,11

---
+++ src/lesson5.java
@@ -0,0 +1,7 @@
+public class lesson5 {
+    public static void main(String[] args) {
+        for(float i = 0f;i<=10;i = i+0.4f){
+            System.out.println("ZDAROVA "+i);
+        }
+    }
+}
---
+++ src/lesson6.java
@@ -0,0 +1,12 @@
+public class lesson6 {
+    public static void main(String[] args) {
+        int myInt = 15;
+        if(myInt<10){
+            System.out.println("верно");
+        } else if(myInt>20) {
+            System.out.println("НЕ верно ");
+        } else{
+            System.out.println("от 10 до 20");
+        }
+    }
+}
