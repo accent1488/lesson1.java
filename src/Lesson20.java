42f118ac6a37b660b4c6c11093d2357e7abedc1f
accent1488 <rkfldkd@gmail.com>
2021-07-17 19:32:54 +0400

    lesson21

--- src/Lesson20.java
+++ src/Lesson20.java
@@ -1,6 +1,13 @@
 public class Lesson20 {
     public static void main(String[] args) {
-    Human human1 = new Human();
+        Human h1 = new Human("Bob",44);
+        Human h2 = new Human("Top",24);
+        h1.printNumberOfPeople();
+        h2.printNumberOfPeople();
+        Human h3 = new Human("Tee",22);
+        h1.printNumberOfPeople();
+        h2.printNumberOfPeople();
+        h3.printNumberOfPeople();
     }
 }
 
@@ -8,24 +15,17 @@ class Human{
 
     private String name;
     private int age;
+    private static int countPeople = 0;
 
-    public Human(){
-        this.name = "имя по умолчанию";
-        this.age = 0;
-    }
-
-    public Human(String name){
-        System.out.println("привет из второго конструктора!");
-        this.name = name;
-    }
 
     public Human(String name, int age){
         System.out.println("Привет из третьего конструктора!");
         this.name = name;
         this.age = age;
+        countPeople++;
     }
-
-    public void setName (String name) { this.name = name; }
-
-    public void setAge (int age) { this.age = age; }
+    public void printNumberOfPeople(){
+        System.out.println("Number of people is "+countPeople);
+    }
 }
+
